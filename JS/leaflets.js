2ebed14566ec8a70410e4c107af076c4a4e02cbf
NatachaVergara <ntchvergara@gmail.com>
2021-08-30 14:22:21 -0300

    tratando de unir el html con el mapa

--- JS/leaflets.js
+++ JS/leaflets.js
@@ -5,32 +5,36 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
 
 
 //Funcion que itera sobre el erray de las dieteticas que esta en el el archivo dieteticasCoodenadas.js
-
+//Creo un arrey markes que se van a ir guardando los marker por separado
+let markers = []
 for (let i = 0; i < diet.length; i++) {
-
-    console.log(diet[i]);
-
+        
     marker = new L.marker([diet[i][0], diet[i][1]]).bindPopup(`<ul class="tiendasPopup">
     <li>${diet[i][2]}</li><li>🏪 ${diet[i][3]}</li><li>☎️ ${diet[i][4]}</li></ul> `).addTo(mymap)
+
+    
+    markers.push(marker)
+ 
+
+
 }
 
-//traigo el link Ver en mapa
+
+//traigo el html Ver en mapa
 const links = document.querySelectorAll('.links');
 
-// for (let a = 0; a < links.length; a++){
 
-//     links[a].addEventListener('click', ()=>{
-//         marker.openPopup()
-//     })
-
-
-// }
 
 //creo un loop para que en cada links, se cree un link y su posicion
-links.forEach((link,i)=>{
-    link.addEventListener('click', ()=>{
-        marker.openPopup(link[i])
-    })
-})
+links.forEach((link, index) => {
+    link.addEventListener('click', () => 
+    {
+        marker.openPopup(markers[index])
+        }
+    
 
+    )
+
+
+})
 
