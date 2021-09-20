4ffbac315c3425253f8547c67d2a81ae59bc02b2
Brian Paul Salinas <braiiann.1998@gmail.com>
2021-09-20 20:38:34 -0300

    contacto terminado

--- models/contacto.js
+++ models/contacto.js
@@ -1,6 +1,7 @@
 const {request} = require('../db/mysql')
 
 module.exports.contact = async (name,email,cel,msj) =>{
+      
     const user_message = await request(`
     INSERT INTO contact (name, email, cel_phone, message) VALUES('${name}','${email}','${cel}','${msj}')
     
