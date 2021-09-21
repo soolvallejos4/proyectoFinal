be258ee59cadc3bc060ba6197dac5bc8c9d638fc
NatachaVergara <ntchvergara@gmail.com>
2021-09-20 23:16:37 -0300

    actualizando cosas

--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,30 @@
 //--------toggle navbar------
-const navToggler = document.querySelector(".navbar-toggler");
-navToggler.addEventListener("click", toggleNav)
+// const navToggler = document.querySelector(".navbar-toggler");
+// navToggler.addEventListener("click", toggleNav)
 
-function toggleNav() {
-    navToggler.classList.toggle("active");
-    document.querySelector(".nav").classList.toggle("open");
-}
+// function toggleNav() {
+//     navToggler.classList.toggle("active");
+//     document.querySelector(".nav").classList.toggle("open");
+// }
 
-// CERRANDO NAVEGADOR CUANDO HAGO CLICK
+// // CERRANDO NAVEGADOR CUANDO HAGO CLICK
 
-document.addEventListener("click", function (e) {
+// document.addEventListener("click", function (e) {
 
-    if (e.target.closest(".nav-item")) {
-        toggleNav()
-    }
-})
+//     if (e.target.closest(".nav-item")) {
+//         toggleNav()
+//     }
+// })
 
-//-------------sticky hader------------------------------
-window.addEventListener("scroll", function () {
-    if (this.pageXOffset > 60) {
-        document.querySelector(".header").classList.add("sticky");
-    } else {
-        document.querySelector(".header").classList.remove("sticky");
-    }
-})
+// //-------------sticky hader------------------------------
+// window.addEventListener("scroll", function () {
+//     if (this.pageXOffset > 60) {
+//         document.querySelector(".header").classList.add("sticky");
+//     } else {
+//         document.querySelector(".header").classList.remove("sticky");
+//     }
+// })
 
 
-//
+
 
